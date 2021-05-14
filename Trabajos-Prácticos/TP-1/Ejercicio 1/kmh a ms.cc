d5144d8cf685fd63681e1006111be72f26c022ba
Pedro Baccaro <pbaccaro@frba.utn.edu.ar>
2021-05-13 23:15:48 -0300

    agregué ejercicio 1

---
+++ Trabajos-Prácticos/TP-1/Ejercicio 1/kmh a ms.cc
@@ -0,0 +1,30 @@
+/*
+Consigna:
+Realizar un programa que lea una velocidad en km/hr y la transforme a
+mts/seg.. El programa debe solicitar por pantalla un valor numérico, el luego
+el usuario debe ingresar un valor, finalmente el programa debe mostrar por
+pantalla el resultado de la conversión.
+
+*/
+
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "spanish");
+
+    float velocidadKMHR, velocidadMS;
+
+    cout << "Por favor, ingrese una velocidad en Km/Hr:" << endl;
+    cin >> velocidadKMHR;
+
+    velocidadMS = velocidadKMHR/3.6;
+
+    cout << velocidadKMHR << "Km/Hr = " << velocidadMS << "M/S." <<endl;
+    
+    system("pause");
+    return 0;
+}
